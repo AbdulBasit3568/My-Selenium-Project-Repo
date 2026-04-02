5a3fd2a5f5a298338db6b0ae09375f233c6e66a8
Abdul Basit <basitsandhu3568@gmail.com>
2026-04-02 11:51:55 +0500

    Remove project files from repository

--- Day2/Lab001 Opn & prnt.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-driver = webdriver.Chrome()
-driver.get("https://cloud.tarsil.pk")
-driver.maximize_window()
-print(driver.title)
-print(driver.current_url)
-time.sleep(5)
-driver.quit()
